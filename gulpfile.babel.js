4877b65c09b6ddc8cb779c7c44898e8fac5cc3dd
Chris Goostry <c.goostry@staffs.ac.uk>
2024-06-11 16:17:07 +0100

    Daily check in

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -85,6 +85,9 @@ fractal.components.set('default.context', {
   'loremLong': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
   'loremShort': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
   'pangram': 'The quick brown fox jumps over the lazy dog',
+  'quoteText': 'After coming to Staffs, it feels like my future looks different, very bright in fact.',
+  'quoteSource': 'Zenith Shaileshkumar Vaghasiya',
+  'quoteQualification': 'MA Sports Journalism graduate, 2020',
   main_menu: {
     "items": [
       {
